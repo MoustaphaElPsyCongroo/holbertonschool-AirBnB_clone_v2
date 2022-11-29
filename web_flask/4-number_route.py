28a21f0b78d373171f1570cc901c1a95e4c8fe0f
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-29 15:26:28 +0100

    4 - Integer route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Integer module"""
+"""Integer route"""
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route("/")
@@ -38,4 +37,5 @@ def num(n):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.url_map.strict_slashes = False
+    app.run("0.0.0.0", 5000, use_reloader=True)
