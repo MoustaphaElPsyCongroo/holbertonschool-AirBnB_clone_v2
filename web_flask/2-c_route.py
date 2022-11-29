a329634e0c800971726f27e3b3cb68c9670560e8
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-29 15:28:58 +0100

    3

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,7 +2,6 @@
 """Flask web app with 3 routes & one variable"""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/')
@@ -24,4 +23,5 @@ def c_is_fun(text):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, use_reloader=True)
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
