c056d809806abbb545eb24a7762756fca03f1701
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-29 14:18:32 +0100

    2 - Start a Flask web app with one variable route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Flask web app with 3 routes, one variable"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Prints Hello HBNB"""
+    return "Hello HBNB"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Prints HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Prints the <text> variable"""
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", use_reloader=True)
