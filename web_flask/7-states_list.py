0111e7eca7d7c935f68fdf2c086b82902adb16f5
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-11-30 14:08:32 +0100

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,4 +19,4 @@ def teardown_db(exception):
 
 if __name__ == "__main__":
     app.url_map.strict_slashes = False
-    app.run("0.0.0.0", 5000, use_reloader=True)
+    app.run(host="0.0.0.0", port=5000, use_reloader=True)
